dea2d567aed1a56bcf57ba63dfe782d6575c1a78
Somshubra Majumdar <titu1994@gmail.com>
2014-03-26 23:19:30 +0530

    LRU Completed

--- College/src/college/LRUScheduling.java
+++ College/src/college/LRUScheduling.java
@@ -18,36 +18,24 @@ public class LRUScheduling {
 		String data[] = bb.readLine().split(" ");
 
 		for(int i = 0; i < data.length; i++){
-			queue.put(i, data[i]);
+			queue.put(Integer.parseInt(data[i]), data[i]);
 			System.out.println(queue);
 		}
 		System.out.println("Final : " + queue);
 	}
 
 	public static class LRUCache <K, V> extends LinkedHashMap < K, V > {
-		
-		private static final long serialVersionUID = -8468306640924288252L;
 		private int capacity;
 
 		public LRUCache(int capacity) { 
-			super(capacity+1, 1.0f, true);
+			super(capacity, .75f, true);
 			this.capacity = capacity;
 		}
 		
 		protected boolean removeEldestEntry(Entry<K,V> entry) {
 			return (size() > this.capacity);
 		}
-
-		@Override
-		public V put(K key, V value) {
-			if(!values().contains(value))
-				return super.put(key, value);
-			else{
-				get(key);
-				return null;
-			}
-		}
-
+		
 		@Override
 		public String toString() {
 			return values().toString();
